dc27a50086d88f7abce3188bbdcb6613de6bfdf5
Park, Daekyeong <pdk2037@gmail.com>
2025-10-27 23:42:22 +0900

    feat: 이름 최대 길이 제한 예외 처리

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -1,11 +1,20 @@
 package racingcar.domain;
 
 public class Car {
+    private static final int MAX_NAME_LENGTH = 5;
+
     private String name;
     private int moveCount = 0;
 
     public Car(String name) {
         this.name = name;
+        validateName(name);
+    }
+
+    public static void validateName(String name) {
+        if (name.length() > MAX_NAME_LENGTH) {
+            throw new IllegalArgumentException("이름은 5자를 초과할 수 없습니다.");
+        }
     }
 
 }
